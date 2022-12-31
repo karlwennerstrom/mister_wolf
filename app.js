ad240c369433e8b042aa3e3e0914ee8a1265b3ed
Karl Wennerstrom <karlheinzwennerstrom@gmail.com>
2022-12-31 00:09:56 -0300

    first commit

--- app.js
+++ app.js
@@ -12,14 +12,25 @@ const session = require('express-session')
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use(cors())
+app.set('trust proxy', 1);
+
 app.use(session({
+cookie:{
+    secure: true,
+    maxAge:60000
+       },
+store: new RedisStore(),
+secret: 'secret',
+saveUninitialized: true,
+resave: false
+}));
 
-    secret:'Karl241988',
-    resave:true,
-    saveUninitialized:true
-
-}))
-
+app.use(function(req,res,next){
+if(!req.session){
+    return next(new Error('Oh no')) //handle error
+}
+next() //otherwise continue
+});
 
 
 
